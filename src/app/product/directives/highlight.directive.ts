a6835ac9976502b51f88e88c62f74765af7e61d8
bngams <n.boris@devidia.net>
2018-12-19 14:22:43 +0100

    HostBinding

--- src/app/product/directives/highlight.directive.ts
+++ src/app/product/directives/highlight.directive.ts
@@ -1,12 +1,20 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
+  @Input('appHighlight')
+  isHighlighted = false;
+
   constructor(element: ElementRef) {
-    element.nativeElement.style.backgroundColor = 'yellow';
+    // element.nativeElement.style.backgroundColor = 'yellow';
+  }
+
+  @HostBinding('class.highlighted')
+  get cssClass() {
+    return this.isHighlighted;
   }
 
 }
